5c036bc29007735feb448005236e6d43b7da25fa
kassadube <noamsrb@yahoo.com>
2024-04-28 17:54:46 +0300

    add repository

---
+++ HomeManger.Data/CategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeManger.Data
+{
+    public class CategoryRepository: BaseRepository, ICategoryRepository
+    {
+        public CategoryRepository(IConfiguration configuration, ILogger<CategoryRepository> logger) : base(configuration, logger)
+        {
+
+        }
+
+    }
+}
---
+++ HomeManger.Data/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeManger.Data
+{
+    public interface ICategoryRepository
+    {
+    }
+}
